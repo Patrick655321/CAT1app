1423514664baabc5baf1a10fd436c931625f4d04
Patrick655321 <12684@coderacademy.edu.au>
2022-07-15 12:34:55 +1000

    Noclass shift calculation formula working

---
+++ noclassfunc.py
@@ -0,0 +1,6 @@
+
+def roster_intro():
+    print("Welcome to RosterRight, your one-stop anachronistic rostering service that noone will probably use!")
+    print("According to shift prerequisites the following shifts require staff:")
+
+roster_intro()
---
+++ noclassgroup.py
@@ -0,0 +1,65 @@
+# Moe = Staff("mgr", "both", "both", "both", "both", "both", "both", "both")
+# Grace = Staff("mgr", "both", "both", "both", "both", "both", "both", "both")
+# Judith = Staff("mgr", "both", "both", "both", "both", "both", "both", "both")
+# Luane = Staff("mgr", "both", "both", "both", "both", "both", "both", "both")
+# Fausto = Staff("mgr", "both", "both", "both", "both", "both", "both", "both")
+# George = Staff("mgr", "both", "both", "both", "both", "both", "both", "both")
+# Paul = Staff("mgr", "both", "both", "both", "both", "both", "both", "both")
+# Mykel = Staff("mgr", "both", "both", "both", "both", "both", "both", "both")
+# Mustafa = Staff("mgr", "both", "both", "both", "both", "both", "both", "both")
+# Maddison = Staff("mgr", "both", "both", "both", "both", "both", "both", "both")
+# Allie = Staff("mgr", "both", "both", "both", "both", "both", "both", "both")
+
+staff_avail = {
+    "Moe" : "both",
+    "Grace" : "both",
+    "Judith" : "both",
+    "Fausto" : "both",
+    "George" : "both",
+    "Paul" : "both",
+    "Mykel" : "both",
+    "Mustafa" : "both",
+    "Allie" : "both",
+    "Mustafa" : "both"
+}
+
+
+mon_day_staff = {
+    "bar" : 0,
+    "rest" : 0,
+    "mgr" : 0,
+}
+
+mon_night_staff  = []
+
+week_day_shift = {
+    "bar" : 1,
+    "rest" : 2,
+    "mgr" : 1,
+}
+
+def testing():
+    if mon_day_staff != week_day_shift:
+        print(f"""Bar {week_day_shift["bar"] - mon_day_staff["bar"]}""")
+
+testing()
+
+week_night = {
+    "bar" : 2,
+    "rest" : 3,
+    "mgr" : 2,
+}
+
+frisat_day = {
+    "bar" : 2,
+    "rest" : 4,
+    "mgr" : 2,
+}
+
+frisat_night = {
+    "bar" : 3,
+    "rest" : 5,
+    "mgr" : 2,
+}
+
+
